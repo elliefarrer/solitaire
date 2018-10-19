eb06c4fe8185293b808eddeb39828453a2d7b388
Ellie Farrer <elliefarrermusic@gmail.com>
2018-10-19 09:04:07 +0100

    The correct cards are flipped in their columns

--- js/app.js
+++ js/app.js
@@ -16,61 +16,61 @@ const pushToArray = (array, newItem) => {
   array.push(newItem);
 };
 
-pushToArray(deckOfCards, new CreateDeck(0, 'King of Hearts', 'king', 'hearts', 13));
-pushToArray(deckOfCards, new CreateDeck(1, 'Queen of Hearts', 'queen', 'hearts', 12));
-pushToArray(deckOfCards, new CreateDeck(2, 'Jack of Hearts', 'jack', 'hearts', 11));
-pushToArray(deckOfCards, new CreateDeck(3, 'Ten of Hearts', 'ten', 'hearts', 10));
-pushToArray(deckOfCards, new CreateDeck(4, 'Nine of Hearts', 'nine', 'hearts', 9));
-pushToArray(deckOfCards, new CreateDeck(5, 'Eight of Hearts', 'eight', 'hearts', 8));
-pushToArray(deckOfCards, new CreateDeck(6, 'Seven of Hearts', 'seven', 'hearts', 7));
-pushToArray(deckOfCards, new CreateDeck(7, 'Six of Hearts', 'six', 'hearts', 6));
-pushToArray(deckOfCards, new CreateDeck(8, 'Five of Hearts', 'five', 'hearts', 5));
-pushToArray(deckOfCards, new CreateDeck(9, 'Four of Hearts', 'four', 'hearts', 4));
-pushToArray(deckOfCards, new CreateDeck(10, 'Three of Hearts', 'three', 'hearts', 3));
-pushToArray(deckOfCards, new CreateDeck(11, 'Two of Hearts', 'two', 'hearts', 2));
-pushToArray(deckOfCards, new CreateDeck(12, 'Ace of Hearts', 'ace', 'hearts', 1));
+pushToArray(deckOfCards, new CreateDeck(0, 'K♥︎', 'king', 'hearts', 13));
+pushToArray(deckOfCards, new CreateDeck(1, 'Q♥︎', 'queen', 'hearts', 12));
+pushToArray(deckOfCards, new CreateDeck(2, 'J♥︎', 'jack', 'hearts', 11));
+pushToArray(deckOfCards, new CreateDeck(3, '10♥︎', 'ten', 'hearts', 10));
+pushToArray(deckOfCards, new CreateDeck(4, '9♥︎', 'nine', 'hearts', 9));
+pushToArray(deckOfCards, new CreateDeck(5, '8♥︎', 'eight', 'hearts', 8));
+pushToArray(deckOfCards, new CreateDeck(6, '7♥︎', 'seven', 'hearts', 7));
+pushToArray(deckOfCards, new CreateDeck(7, '6♥︎', 'six', 'hearts', 6));
+pushToArray(deckOfCards, new CreateDeck(8, '5♥︎', 'five', 'hearts', 5));
+pushToArray(deckOfCards, new CreateDeck(9, '4♥︎', 'four', 'hearts', 4));
+pushToArray(deckOfCards, new CreateDeck(10, '3♥︎', 'three', 'hearts', 3));
+pushToArray(deckOfCards, new CreateDeck(11, '2♥︎', 'two', 'hearts', 2));
+pushToArray(deckOfCards, new CreateDeck(12, 'A♥︎', 'ace', 'hearts', 1));
 
-pushToArray(deckOfCards, new CreateDeck(13, 'King of Diamonds', 'king', 'diamonds', 13));
-pushToArray(deckOfCards, new CreateDeck(14, 'Queen of Diamonds', 'queen', 'diamonds', 12));
-pushToArray(deckOfCards, new CreateDeck(15, 'Jack of Diamonds', 'jack', 'diamonds', 11));
-pushToArray(deckOfCards, new CreateDeck(16, 'Ten of Diamonds', 'ten', 'diamonds', 10));
-pushToArray(deckOfCards, new CreateDeck(17, 'Nine of Diamonds', 'nine', 'diamonds', 9));
-pushToArray(deckOfCards, new CreateDeck(18, 'Eight of Diamonds', 'eight', 'diamonds', 8));
-pushToArray(deckOfCards, new CreateDeck(19, 'Seven of Diamonds', 'seven', 'diamonds', 7));
-pushToArray(deckOfCards, new CreateDeck(20, 'Six of Diamonds', 'six', 'diamonds', 6));
-pushToArray(deckOfCards, new CreateDeck(21, 'Five of Diamonds', 'five', 'diamonds', 5));
-pushToArray(deckOfCards, new CreateDeck(22, 'Four of Diamonds', 'four', 'diamonds', 4));
-pushToArray(deckOfCards, new CreateDeck(23, 'Three of Diamonds', 'three', 'diamonds', 3));
-pushToArray(deckOfCards, new CreateDeck(24, 'Two of Diamonds', 'two', 'diamonds', 2));
-pushToArray(deckOfCards, new CreateDeck(25, 'Ace of Diamonds', 'ace', 'diamonds', 1));
+pushToArray(deckOfCards, new CreateDeck(13, 'K♦︎', 'king', 'diamonds', 13));
+pushToArray(deckOfCards, new CreateDeck(14, 'Q♦︎', 'queen', 'diamonds', 12));
+pushToArray(deckOfCards, new CreateDeck(15, 'J♦︎', 'jack', 'diamonds', 11));
+pushToArray(deckOfCards, new CreateDeck(16, '10♦︎', 'ten', 'diamonds', 10));
+pushToArray(deckOfCards, new CreateDeck(17, '9♦︎', 'nine', 'diamonds', 9));
+pushToArray(deckOfCards, new CreateDeck(18, '8♦︎', 'eight', 'diamonds', 8));
+pushToArray(deckOfCards, new CreateDeck(19, '7♦︎', 'seven', 'diamonds', 7));
+pushToArray(deckOfCards, new CreateDeck(20, '6♦︎', 'six', 'diamonds', 6));
+pushToArray(deckOfCards, new CreateDeck(21, '5♦︎', 'five', 'diamonds', 5));
+pushToArray(deckOfCards, new CreateDeck(22, '4♦︎', 'four', 'diamonds', 4));
+pushToArray(deckOfCards, new CreateDeck(23, '3♦︎', 'three', 'diamonds', 3));
+pushToArray(deckOfCards, new CreateDeck(24, '2♦︎', 'two', 'diamonds', 2));
+pushToArray(deckOfCards, new CreateDeck(25, 'A♦︎', 'ace', 'diamonds', 1));
 
-pushToArray(deckOfCards, new CreateDeck(26, 'King of Spades', 'king', 'spades', 13));
-pushToArray(deckOfCards, new CreateDeck(27, 'Queen of Spades', 'queen', 'spades', 12));
-pushToArray(deckOfCards, new CreateDeck(28, 'Jack of Spades', 'jack', 'spades', 11));
-pushToArray(deckOfCards, new CreateDeck(29, 'Ten of Spades', 'ten', 'spades', 10));
-pushToArray(deckOfCards, new CreateDeck(30, 'Nine of Spades', 'nine', 'spades', 9));
-pushToArray(deckOfCards, new CreateDeck(31, 'Eight of Spades', 'eight', 'spades', 8));
-pushToArray(deckOfCards, new CreateDeck(32, 'Seven of Spades', 'seven', 'spades', 7));
-pushToArray(deckOfCards, new CreateDeck(33, 'Six of Spades', 'six', 'spades', 6));
-pushToArray(deckOfCards, new CreateDeck(34, 'Five of Spades', 'five', 'spades', 5));
-pushToArray(deckOfCards, new CreateDeck(35, 'Four of Spades', 'four', 'spades', 4));
-pushToArray(deckOfCards, new CreateDeck(36, 'Three of Spades', 'three', 'spades', 3));
-pushToArray(deckOfCards, new CreateDeck(37, 'Two of Spades', 'two', 'spades', 2));
-pushToArray(deckOfCards, new CreateDeck(38, 'Ace of Spades', 'ace', 'spades', 1));
+pushToArray(deckOfCards, new CreateDeck(26, 'K♠︎', 'king', 'spades', 13));
+pushToArray(deckOfCards, new CreateDeck(27, 'Q♠︎', 'queen', 'spades', 12));
+pushToArray(deckOfCards, new CreateDeck(28, 'J♠︎', 'jack', 'spades', 11));
+pushToArray(deckOfCards, new CreateDeck(29, '10♠︎', 'ten', 'spades', 10));
+pushToArray(deckOfCards, new CreateDeck(30, '9♠︎', 'nine', 'spades', 9));
+pushToArray(deckOfCards, new CreateDeck(31, '8♠︎', 'eight', 'spades', 8));
+pushToArray(deckOfCards, new CreateDeck(32, '7♠︎', 'seven', 'spades', 7));
+pushToArray(deckOfCards, new CreateDeck(33, '6♠︎', 'six', 'spades', 6));
+pushToArray(deckOfCards, new CreateDeck(34, '5♠︎', 'five', 'spades', 5));
+pushToArray(deckOfCards, new CreateDeck(35, '4♠︎', 'four', 'spades', 4));
+pushToArray(deckOfCards, new CreateDeck(36, '3♠︎', 'three', 'spades', 3));
+pushToArray(deckOfCards, new CreateDeck(37, '2♠︎', 'two', 'spades', 2));
+pushToArray(deckOfCards, new CreateDeck(38, 'A♠︎', 'ace', 'spades', 1));
 
-pushToArray(deckOfCards, new CreateDeck(39, 'King of Clubs', 'king', 'clubs', 13));
-pushToArray(deckOfCards, new CreateDeck(40, 'Queen of Clubs', 'queen', 'clubs', 12));
-pushToArray(deckOfCards, new CreateDeck(41, 'Jack of Clubs', 'jack', 'clubs', 11));
-pushToArray(deckOfCards, new CreateDeck(42, 'Ten of Clubs', 'ten', 'clubs', 10));
-pushToArray(deckOfCards, new CreateDeck(43, 'Nine of Clubs', 'nine', 'clubs', 9));
-pushToArray(deckOfCards, new CreateDeck(44, 'Eight of Clubs', 'eight', 'clubs', 8));
-pushToArray(deckOfCards, new CreateDeck(45, 'Seven of Clubs', 'seven', 'clubs', 7));
-pushToArray(deckOfCards, new CreateDeck(46, 'Six of Clubs', 'six', 'clubs', 6));
-pushToArray(deckOfCards, new CreateDeck(47, 'Five of Clubs', 'five', 'clubs', 5));
-pushToArray(deckOfCards, new CreateDeck(48, 'Four of Clubs', 'four', 'clubs', 4));
-pushToArray(deckOfCards, new CreateDeck(49, 'Three of Clubs', 'three', 'clubs', 3));
-pushToArray(deckOfCards, new CreateDeck(50, 'Two of Clubs', 'two', 'clubs', 2));
-pushToArray(deckOfCards, new CreateDeck(51, 'Ace of Clubs', 'ace', 'clubs', 1));
+pushToArray(deckOfCards, new CreateDeck(39, 'K♣︎', 'king', 'clubs', 13));
+pushToArray(deckOfCards, new CreateDeck(40, 'Q♣︎', 'queen', 'clubs', 12));
+pushToArray(deckOfCards, new CreateDeck(41, 'J♣︎', 'jack', 'clubs', 11));
+pushToArray(deckOfCards, new CreateDeck(42, '10♣︎', 'ten', 'clubs', 10));
+pushToArray(deckOfCards, new CreateDeck(43, '9♣︎', 'nine', 'clubs', 9));
+pushToArray(deckOfCards, new CreateDeck(44, '8♣︎', 'eight', 'clubs', 8));
+pushToArray(deckOfCards, new CreateDeck(45, '7♣︎', 'seven', 'clubs', 7));
+pushToArray(deckOfCards, new CreateDeck(46, '6♣︎', 'six', 'clubs', 6));
+pushToArray(deckOfCards, new CreateDeck(47, '5♣︎', 'five', 'clubs', 5));
+pushToArray(deckOfCards, new CreateDeck(48, '4♣︎', 'four', 'clubs', 4));
+pushToArray(deckOfCards, new CreateDeck(49, '3♣︎', 'three', 'clubs', 3));
+pushToArray(deckOfCards, new CreateDeck(50, '2♣︎', 'two', 'clubs', 2));
+pushToArray(deckOfCards, new CreateDeck(51, 'A♣︎', 'ace', 'clubs', 1));
 
 const shuffleCards = () => {
   deckOfCards = deckOfCards.sort(() => .5 - Math.random());
@@ -99,6 +99,14 @@ pushToArray(columns, new CreateColumns(6, 'column 6', 6, 60, []));
 pushToArray(columns, new CreateColumns(7, 'column 7', 7, 70, []));
 
 console.log('Columns are', columns);
+
+const flipCard = card => {
+  if(!card.showFront) {
+    card.showFront = true;
+    card.addClass('front');
+  }
+};
+
 $(() => {
   const $columnsContainer = $('.columns-container');
 
@@ -112,9 +120,13 @@ $(() => {
     for (let cardIndex = 0; cardIndex < column.initialNumber; cardIndex++) {
       console.log(cardIndex);
       const $newCard = $(`<div class="card" style="top: ${cardIndex * 200}%">${deckOfCards[cardIndex].name}</div>`);
+      column.cards.push($newCard);
       $column.append($newCard);
       deckOfCards = deckOfCards.filter(card => card.name !== deckOfCards[cardIndex].name);
     }
+    console.log('The column has', column.cards);
+
+    flipCard(column.cards[column.cards.length - 1]);
   });
 
   console.log('Deck of cards now contains', deckOfCards);
